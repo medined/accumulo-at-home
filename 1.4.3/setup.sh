0de842efaf9504dfb5da0f9fe2a59a187db11ec2
David Medinets <david.medinets@gmail.com>
2014-08-17 12:40:00 -0400

    Update setup.sh

--- 1.4.3/setup.sh
+++ 1.4.3/setup.sh
@@ -5,8 +5,29 @@ export BASE_DIR=$HOME/accumulo_home
 export ACCUMULO_HOME=$BASE_DIR/bin/accumulo
 export HADOOP_HOME=$BASE_DIR/bin/hadoop
 export HADOOP_PREFIX=$BASE_DIR/bin/hadoop
-export JAVA_HOME=/usr/lib/jvm/java-6-openjdk
 export ZOOKEEPER_HOME=$BASE_DIR/bin/zookeeper
+
+if [ -d /usr/lib/jvm/java-6-openjdk ];
+then
+  # Ubuntu 10.04 (and perhaps others)
+  JAVA_HOME=/usr/lib/jvm/java-6-openjdk
+  echo "$0: Set java home to java 6 openjdk"
+fi
+if [ -d /usr/lib/jvm/java-6-openjdk-i386 ];
+then
+  JAVA_HOME=/usr/lib/jvm/java-6-openjdk-i386
+  echo "$0: Set java home to java 6 openjdk-i386"
+fi
+if [ -d /usr/lib/jvm/java-7-openjdk-amd64 ];
+then
+  JAVA_HOME=/usr/lib/jvm/java-7-openjdk-amd64/jre
+  echo "$0: Set java home to java 7 openjdk"
+fi
+if [ "$JAVA_HOME" == "" ];
+then
+  echo "$0: UNABLE TO SET JAVA_HOME!"
+  exit 1
+fi
 
 export HADOOP_VERSION=hadoop-1.0.4
 export ZOOKEEPER_VERSION=zookeeper-3.4.3
